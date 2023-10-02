90b492cf6773856e108623997d959944a0c31f1f
smalltechh <engineering709@gmail.com>
2023-10-02 16:29:36 -0700

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/tests/9-main.py
+++ 0x08-python-more_classes/tests/9-main.py
@@ -4,12 +4,3 @@ Rectangle = __import__('9-rectangle').Rectangle
 my_square = Rectangle.square(5)
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
 print(my_square)
-
-guillaume@ubuntu:~/0x08$ ./9-main.py
-Area: 25 - Perimeter: 20
-#####
-#####
-#####
-#####
-#####
-Bye rectangle...
